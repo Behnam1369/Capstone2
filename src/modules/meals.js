f74c0fcc01e7382c225280a0cecad9b776d97f63
Behnam Aghaali <Behnam.Aghaali@yahoo.com>
2022-03-09 23:41:26 +0330

    Add comments popup

--- src/modules/meals.js
+++ src/modules/meals.js
@@ -1,6 +1,6 @@
 const getMeals = () => {
   const meals = fetch('https://www.themealdb.com/api/json/v1/1/filter.php?a=chinese')
-    .then((response) => response.text())
+    .then((response) => response.json())
     .then((result) => result)
     .catch((error) => `failed.${error}`);
 
