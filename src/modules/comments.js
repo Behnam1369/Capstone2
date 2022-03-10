9efaf7bb8110054c8a35de7cb3b37c641438c0e0
Behnam Aghaali <Behnam.Aghaali@yahoo.com>
2022-03-11 01:19:59 +0330

    Add a function for counting comments for each meal

--- src/modules/comments.js
+++ src/modules/comments.js
@@ -1,3 +1,5 @@
+import commentsCount from './commentCount.js';
+
 const appid = 'tLVd7tb0BQpHYLFSGTqK';
 
 const getComments = (idmeal) => {
@@ -32,10 +34,9 @@ const showPopup = async (idmeal) => {
   message.classList.add('message');
 
   const showComments = () => {
-    let res = '';
+    let res = `<h2>Comments (${commentsCount(comments)})</h2>`;
     if (comments.length > 0) {
-      res = `
-  <h2>Comments</h2>
+      res += ` 
   <div class="comments">${comments.map((el) => `<div class='comment'>
       <div class="user"> ${el.username} <span>${el.creation_date}</span> </div>
       <p>${el.comment}</p>
@@ -75,18 +76,25 @@ const showPopup = async (idmeal) => {
     };
 
     message.innerHTML = '';
+    btnSubmitComment.disabled = true;
+    btnSubmitComment.innerHTML = 'Submitting ... ';
     fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appid}/comments`, requestOptions)
       .then((response) => response.text())
       .then(async (result) => {
         if (result === 'Created') {
           message.classList.add('success');
-          message.innerHTML = 'Your comment saved suseccfully.';
+          message.innerHTML = 'Your comment was saved suseccfully.';
           comments = await getComments(idmeal);
           commentsDiv.innerHTML = showComments();
         } else {
           message.classList.add('failed');
           message.innerHTML = 'An error accured. Please try again later';
         }
+      }).finally(() => {
+        txtUser.value = '';
+        txtComment.value = '';
+        btnSubmitComment.disabled = false;
+        btnSubmitComment.innerHTML = 'Submit your comment';
       });
 
     return null;
@@ -122,7 +130,7 @@ const showPopup = async (idmeal) => {
   infoDiv.innerHTML = `
   <h2>${mealDetail.meals[0].strMeal}</h2>
   ${mealDetail.meals[0].strInstructions.split(/STEP [0-9] -/).map((el, i) => (i > 0 ? `<p class='instruction'>${i} - ${el}</p>` : '')).join('')}
-  <div class="tags">${(mealDetail.meals[0].strTags || '').split(',').map((el) => `<div class='tag'>${el}</div>`)}</div>
+  <div class="tags">${(mealDetail.meals[0].strTags || '').split(',').map((el) => (el !== '' ? `<div class='tag'>${el}</div>` : '')).join('')}</div>
   `;
   popup.appendChild(infoDiv);
 
